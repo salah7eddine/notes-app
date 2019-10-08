8bd1fc98fa16cfeb103f3a5b6e7b82bcbcef6912
ABOUSAID <salah-eddine.abousaid@capgemini.com>
2019-10-08 15:20:05 +0100

    second

---
+++ app.js
@@ -0,0 +1,12 @@
+// const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', 'This file was created by Node.js.');
+
+// fs.appendFileSync('notes.txt', ' My name is Salah-Eddine');
+
+
+const add = require('./utils');
+
+const sum = add(4, -7);
+
+console.log(sum);
---
+++ utils.js
@@ -0,0 +1,11 @@
+console.log('utils.js');
+
+const name = 'Hamza';
+
+// const add = function (a, b) {
+//     return a + b;
+// }
+
+const add = (a, b) => a + b;
+
+module.exports = add;
